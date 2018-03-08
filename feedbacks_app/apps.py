fffd25b7dee4c189e07be3e128b4eb3306011794
chocolateslovemeg <megha.mca16.du@gmail.com>
2018-03-09 01:46:54 +0530

    postgre connection & apps added

---
+++ feedbacks_app/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class FeedbacksAppConfig(AppConfig):
+    name = 'feedbacks_app'
---
+++ performace_app/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class PerformaceAppConfig(AppConfig):
+    name = 'performace_app'
