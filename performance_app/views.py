784400aa6c2e45dbd7b95918038d3113acc63e08
chocolateslovemeg <megha.mca16.du@gmail.com>
2018-03-22 15:40:40 +0530

    traits passed to html page[performance]

--- performance_app/views.py
+++ performance_app/views.py
@@ -9,14 +9,14 @@ class Trait(ABC):
     def __init__(self,trait_cmp):
         self.trait_component=trait_cmp
 
-    def template_method(self,trait_value_dict):
+    def template_method(self,l1,l2):
         table_name = self.find_table()
         self.store_sid(table_name)
         value=self.calc_value()
-        self.store_value(value,table_name,trait_value_dict)
+        self.store_value(value,table_name,l1,l2)
+        #self.calc_overall_performance()
 
     def find_table(self):
-
         for cls in Base_TraitValueDetails.__subclasses__():
             field=cls._meta.get_field(self.trait_component)
             if field:
@@ -33,10 +33,21 @@ class Trait(ABC):
     def calc_value(self):
         pass
 
-    def store_value(self,value,table_name,trait_value_dict):
+    def store_value(self,value,table_name,l1,l2):
         trait_obj=table_name.objects.filter(sid='ank202').update(**{self.trait_component:value})
-        trait_value_dict[self.trait_component]=value
+        l1.append(self.trait_component)
+        l2.append(value)
         return value
+'''
+    def calc_overall_performance():
+        #class_name = cls.__name__
+        #trait_component = re.sub( '(?<!^)(?=[A-Z])', '_', class_name ).lower()
+        #obj = cls(trait_component)
+        #obj.template_method(trait_value_dict)
+        for cls in Trait.__subclasses__
+            count = count + 1
+            class_name = cls.__name__
+'''
            
 
 class LateShipmentRate(Trait):
@@ -70,18 +81,17 @@ class OnTimeDelivery(Trait):
         return Percentage
 
 def main(request):
-
-    trait_value_dict={}
-
+    l1 = []
+    l2 = []
     for cls in Trait.__subclasses__():
 
         class_name = cls.__name__
         trait_component = re.sub( '(?<!^)(?=[A-Z])', '_', class_name ).lower()
         obj = cls(trait_component)
-        obj.template_method(trait_value_dict)
-
-    return render(request,'performance.html',trait_value_dict)
+        obj.template_method(l1, l2)
+    
+    mylist = zip(l1,l2)
+    return render(request,'performance.html',{'mylist':mylist})
 
 if __name__ == '__main__':
-
     main(request)
