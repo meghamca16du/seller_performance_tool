5485c1baa21a07a1f6863571210bd886dfa7e825
chocolateslovemeg <megha.mca16.du@gmail.com>
2018-03-28 15:49:43 +0530

    feedbacks table bug removed

--- feedbacks_app/models.py
+++ feedbacks_app/models.py
@@ -7,19 +7,19 @@ class FeedbackDetails(models.Model):
     oid = models.ForeignKey('dashboard.OrderDetails', on_delete = models.CASCADE, db_column='oid')
     pid = models.ForeignKey('dashboard.ProductDetails', on_delete = models.CASCADE, db_column='pid')
     feedbackEntered = models.TextField(max_length = 300) 
-    default = '0'
-    oneStar='1'   
-    twoStar='2'
-    threeStar='3'
-    fourStar = '4'
-    fiveStar = '5'
+    default = 0
+    oneStar= 1   
+    twoStar= 2
+    threeStar= 3
+    fourStar = 4
+    fiveStar = 5
     Rating_types=(
-        (default, '0'),
-        (oneStar,'1'),
-        (twoStar,'2'),
-        (threeStar,'3'),
-        (fourStar,'4'),
-        (fiveStar,'5'),
+        (default, 'default'),
+        (oneStar,'oneStar'),
+        (twoStar,'twoStar'),
+        (threeStar,'threeStar'),
+        (fourStar,'fourStar'),
+        (fiveStar,'fiveStar'),
         )
     feedbackRating = models.IntegerField(
         choices = Rating_types,
