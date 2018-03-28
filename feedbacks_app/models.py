6c00dada117d40f9e982742d5210ef2fe9b7fdd8
chocolateslovemeg <megha.mca16.du@gmail.com>
2018-03-28 14:21:42 +0530

    feedback dbtable and class added

--- feedbacks_app/models.py
+++ feedbacks_app/models.py
@@ -1,3 +1,14 @@
 from django.db import models
+from dashboard.models import * 
 
-# Create your models here.
+
+class FeedbackDetails(models.Model):
+    bid = models.ForeignKey('dashboard.BuyerDetails', on_delete = models.CASCADE, db_column='bid')
+    oid = models.ForeignKey('dashboard.OrderDetails', on_delete = models.CASCADE, db_column='oid')
+    pid = models.ForeignKey('dashboard.ProductDetails', on_delete = models.CASCADE, db_column='pid')
+    feedbackEntered = models.TextField(max_length = 300)  
+    dateFeedbackEntered = models.DateField()
+
+    class Meta:
+        managed = True
+        db_table = 'feedback_details'
